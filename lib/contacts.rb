0b659e3de703cc203a7170b278cae7bbac926d01
Patricia Padilha Coleman <patricia.p.coleman@gmail.com>
2019-01-17 18:06:58 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -21,7 +21,7 @@ def remove_strawberry(contacts)
     value.each do |attribute, data|
       
       if attribute == :favorite_ice_cream_flavors
-        binding.pry
+        data.delete_if {|ice_cream| ice_cream == "strawberry"}
       end
       
     end
